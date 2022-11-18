3e05852eecaccd49641e3f3c4fd0216d1163133c
hatsuharu06 <98844415+hatsuharu06@users.noreply.github.com>
2022-11-18 12:37:10 +0700

    Add files via upload

---
+++ 2210631170112_Annisya Dwi Chaerani_Identitas.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+cout<<"          Identitas"<<endl;
+cout<<" "<<endl;
+cout<<"Nama          : Annisya Dwi Chaerani"<<endl;
+cout<<"NPM           : 2210631170112"<<endl;
+cout<<"Alamat        : Karawang"<<endl;
+cout<<"Jenis Kelamin : Perempuan"<<endl;
+cout<<"Hobi          : Nonton Anime"<<endl;
+
+return 0;
+
+}
